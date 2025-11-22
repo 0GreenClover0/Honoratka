07fd49016145da9aa7e5a8a3bd95c3c528c46717
0GreenClover0 <clovers02123@gmail.com>
2025-11-22 20:16:15 +0100

    Everywhere: Add default top-down template project

---
+++ Source/Honoratka/Honoratka.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Honoratka.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Honoratka, "Honoratka" );
+
+DEFINE_LOG_CATEGORY(LogHonoratka)
+ 
---
+++ Source/Honoratka/HonoratkaGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HonoratkaGameMode.h"
+
+AHonoratkaGameMode::AHonoratkaGameMode()
+{
+	// stub
+}
